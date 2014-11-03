ed143e028462e6169421582cabaa9694b17c9750
Denis Zgonjanin <deniszgonjanin@gmail.com>
2014-11-03 11:14:58 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+
+from flask import Flask, request
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.debug = True
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    phone = db.Column(db.String(120))
+    rink_id = db.Column(db.Integer)
+
+    def __init__(self, body, phone):
+        self.body = body
+        self.phone = phone
+
+    def __repr__(self):
+        return '<Message %r:%r>' % (self.phone, self.body)
+
+@app.route('/')
+def hello_world():
+    return 'HomeIce - data collector'
+
+@app.route('/incoming/', methods=['POST'])
+def incoming():
+    body = request.form['Body']
+    phone = request.form['From']
+
+    message = Message(body, phone)
+    db.session.add(message)
+    db.session.commit()
+
+    return ''
+
+port = os.getenv('VCAP_APP_PORT', '5000')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(port))
